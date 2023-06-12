e692178438ae82953c9eeca0fbd2fbc8c9312d87
Your Name <jymohcooler@gmail.com>
2023-06-12 22:43:11 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortcurrentuser = 'G'
+const shortGreeting = `Welcome, ${shortcurrentuser}!`
